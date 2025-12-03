b3b2fd4c0337227087d2a731b31882baa053cce0
takuryto2 <louisguichard761@gmail.com>
2025-12-03 13:45:21 +0100

    quick push

--- Assets/Scripts/Brush/TileBrush.cs
+++ Assets/Scripts/Brush/TileBrush.cs
@@ -9,6 +9,8 @@ public class TileBrush : MonoBehaviour
     [SerializeField] List<SO_Tiles> tiles;
     [SerializeField] List<Tilemap> tilemaps;
     [SerializeField] int radius;
+    [SerializeField] int minRadius;
+    [SerializeField] int maxRadius;
     bool canDraw;
     bool canErase;
     int currentTileindex;
@@ -16,7 +18,6 @@ public class TileBrush : MonoBehaviour
 
     private void Update()
     {
-
         if (canDraw)
         {
             DrawTiles();
@@ -68,32 +69,39 @@ public class TileBrush : MonoBehaviour
         }
     }
 
-    private void DrawTiles()
+    public void ModifyRadius(InputAction.CallbackContext context)
     {
-        CircleDraw();
-        /*Vector3 pos = camera.ScreenToWorldPoint(Input.mousePosition);
-        for (int i = currentTileindex; i >= 0; i--)
+        if (context.ReadValue<int>() < 0 && radius > minRadius)
         {
-            Vector3Int cell = tilemaps[i].WorldToCell(new Vector3(pos.x, pos.y));
-            tilemaps[i].SetTile(cell, tiles[i].RuleTiles);
-        }*/
-    }
-    
-
-    private void EraseTiles()
-    {
-        Vector3 pos = camera.ScreenToWorldPoint(Input.mousePosition);
-        for (int i = tilemaps.Count - 1; i >= 0; i--)
+            radius-=2;
+        }
+        if (context.ReadValue<int>() > 0 && radius < maxRadius)
         {
-            Vector3Int cell = tilemaps[i].WorldToCell(new Vector3(pos.x, pos.y));
-            tilemaps[i].SetTile(cell, null);
+            radius+=2;
         }
     }
 
-    private void CircleDraw()
+    private void DrawTiles()
+    {
+        for (int i = currentTileindex; i >= 0; i--)
+        {
+            CircleDraw(tiles[i].RuleTiles);
+        }
+    }
+    
+    private void EraseTiles()
+    {
+        for (int i = tilemaps.Count - 1; i >= 0; i--)
+        {
+            CircleDraw(null);
+        }
+    }
+
+    private void CircleDraw(RuleTile pRuleTile, bool pIsCircle = false)
     {
         Vector3 pos = camera.ScreenToWorldPoint(Input.mousePosition);
         Vector3Int MidCell = tilemaps[0].WorldToCell(new Vector3(pos.x, pos.y));
+        List<Vector3Int> cirlePositions = new List<Vector3Int>();
         int x = 0;
         int y = radius;
         int p = 1 - radius;
@@ -110,15 +118,101 @@ public class TileBrush : MonoBehaviour
                 p += 2 * (x + y) + 1;
             }
             
-            tilemaps[0].SetTile(new Vector3Int(MidCell.x + x, MidCell.y + y), tiles[0].RuleTiles);
-            tilemaps[0].SetTile(new Vector3Int(MidCell.x - x, MidCell.y + y), tiles[0].RuleTiles);
-            tilemaps[0].SetTile(new Vector3Int(MidCell.x + x, MidCell.y - y), tiles[0].RuleTiles);
-            tilemaps[0].SetTile(new Vector3Int(MidCell.x - x, MidCell.y - y), tiles[0].RuleTiles);
-            tilemaps[0].SetTile(new Vector3Int(MidCell.x + y, MidCell.y + x), tiles[0].RuleTiles);
-            tilemaps[0].SetTile(new Vector3Int(MidCell.x - y, MidCell.y + x), tiles[0].RuleTiles);
-            tilemaps[0].SetTile(new Vector3Int(MidCell.x + y, MidCell.y - x), tiles[0].RuleTiles);
-            tilemaps[0].SetTile(new Vector3Int(MidCell.x - y, MidCell.y - x), tiles[0].RuleTiles);
+            cirlePositions.Add(new Vector3Int(MidCell.x + x, MidCell.y + y));
+            cirlePositions.Add(new Vector3Int(MidCell.x - x, MidCell.y + y));
+            cirlePositions.Add(new Vector3Int(MidCell.x + x, MidCell.y - y));
+            cirlePositions.Add(new Vector3Int(MidCell.x - x, MidCell.y - y));
+            cirlePositions.Add(new Vector3Int(MidCell.x + y, MidCell.y + x));
+            cirlePositions.Add(new Vector3Int(MidCell.x - y, MidCell.y + x));
+            cirlePositions.Add(new Vector3Int(MidCell.x + y, MidCell.y - x));
+            cirlePositions.Add(new Vector3Int(MidCell.x - y, MidCell.y - x));
+
+            #region dessine le cercle
+            
+            tilemaps[0].SetTile(new Vector3Int(MidCell.x + x, MidCell.y + y), pRuleTile);
+            tilemaps[0].SetTile(new Vector3Int(MidCell.x - x, MidCell.y + y), pRuleTile);
+            tilemaps[0].SetTile(new Vector3Int(MidCell.x + x, MidCell.y - y), pRuleTile);
+            tilemaps[0].SetTile(new Vector3Int(MidCell.x - x, MidCell.y - y), pRuleTile);
+            tilemaps[0].SetTile(new Vector3Int(MidCell.x + y, MidCell.y + x), pRuleTile);
+            tilemaps[0].SetTile(new Vector3Int(MidCell.x - y, MidCell.y + x), pRuleTile);
+            tilemaps[0].SetTile(new Vector3Int(MidCell.x + y, MidCell.y - x), pRuleTile);
+            tilemaps[0].SetTile(new Vector3Int(MidCell.x - y, MidCell.y - x), pRuleTile);
+
+            #endregion
+            
             x++;
         }
+        // DrawDisc(cirlePositions, MidCell, ruleTile);
     }
+
+    // private void DrawDisc(List<Vector3Int> cirlePositions,Vector3Int MidCell, RuleTile ruleTile)
+    // {
+    //     foreach (Vector3Int position in cirlePositions)
+    //     {
+    //         if (position.x > MidCell.x && position.y > MidCell.y)
+    //         {
+    //             for (int i = 0; i < radius - 1; i++)
+    //             {
+    //                 tilemaps[0].SetTile(new Vector3Int(position.x - i, position.y), ruleTile);
+    //                 tilemaps[0].SetTile(new Vector3Int(position.x, position.y - i), ruleTile);
+    //             }
+    //         }
+    //         else if (position.x < MidCell.x && position.y > MidCell.y)
+    //         {
+    //             for (int i = 0; i < radius - 1; i++)
+    //             {
+    //                 tilemaps[0].SetTile(new Vector3Int(position.x + i, position.y), ruleTile);
+    //                 tilemaps[0].SetTile(new Vector3Int(position.x, position.y - i), ruleTile);
+    //             }
+    //         }
+    //         else if (position.x > MidCell.x && position.y < MidCell.y)
+    //         {
+    //             for (int i = 0; i < radius - 1; i++)
+    //             {
+    //                 tilemaps[0].SetTile(new Vector3Int(position.x - i, position.y), ruleTile);
+    //                 tilemaps[0].SetTile(new Vector3Int(position.x, position.y + i), ruleTile);
+    //             }
+    //         }
+    //         else if(position.x < MidCell.x && position.y < MidCell.y)
+    //         {
+    //             for (int i = 0; i < radius - 1; i++)
+    //             {
+    //                 tilemaps[0].SetTile(new Vector3Int(position.x + i, position.y), ruleTile);
+    //                 tilemaps[0].SetTile(new Vector3Int(position.x, position.y + i), ruleTile);
+    //             }
+    //         }
+    //     }
+    //     int startPos = (radius%2 == 0) ? radius/2 : radius-1/2;
+    //     Vector3Int startPosVector =  MidCell;
+    //     for (int y = 0; y < radius - 2; y++)
+    //     {
+    //         for (int x = 0; x < radius - 2; x++)
+    //         {
+    //             tilemaps[0].SetTile(new Vector3Int(startPosVector.x - x, startPosVector.y - y), ruleTile);
+    //         }
+    //     }
+    // }
+
+    /*private void LineDraw(Vector3Int pos, Vector3Int target, RuleTile ruleTile)
+    {
+        int dx = target.x - pos.x;
+        int dy = target.y - pos.y;
+
+        if (dx != 0)
+        {
+            int y = pos.y;
+            int p = 2 * dy - dx;
+            for (int i = 0; i < dx+1; i++)
+            {
+                tilemaps[0].SetTile(new Vector3Int(pos.x, Mathf.RoundToInt(pos.y + i), y), ruleTile);
+                
+                if (p >= 0)
+                {
+                    y++;
+                    p -= 2 * dx;
+                }
+                p += 2 * dy;
+            }
+        }
+    }*/
 }
