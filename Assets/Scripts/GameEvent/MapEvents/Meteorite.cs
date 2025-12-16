01aa335d0ec90ebeab2b9f3f07c979f36c746df2
takuryto2 <louisguichard761@gmail.com>
2025-12-16 15:47:50 +0100

    meteorite/tornado sprite

--- Assets/Scripts/GameEvent/MapEvents/Meteorite.cs
+++ Assets/Scripts/GameEvent/MapEvents/Meteorite.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using Random = UnityEngine.Random;
@@ -13,13 +15,20 @@ public class Meteorite : GameEvent
     [SerializeField] GameEventBrush brush;
     [SerializeField] List<Tilemap> tilemaps;
     [SerializeField] SO_Tiles SO_Tile;
+    [SerializeField] GameObject meteoriteEntity;
     float timer;
+
+    private void Awake()
+    {
+        meteoriteEntity.SetActive(false);
+    }
 
     public override void SetupEvent(int x, int y, float pTimer = 0)
     {
         Debug.Log("Setup Meteorite");
         int radius = Random.Range(radiusMinMax.x, radiusMinMax.y);
         int activation = Random.Range(activationMinMax.x, activationMinMax.y);
+        meteoriteEntity.SetActive(true);
         StartCoroutine(StartMeteorite(radius, activation, new Vector3Int(x, y), pTimer));
     }
     
@@ -31,13 +40,16 @@ public class Meteorite : GameEvent
             yield return null;
         }
         timer = 0;
+        meteoriteEntity.transform.position = pLocation+new Vector3Int(10, 10);
+        meteoriteEntity.SetActive(true);
+        meteoriteEntity.transform.DOMove(pLocation, pActivationTimer).SetEase(Ease.InQuint);
         while (timer < pActivationTimer)
         {
-            //jouer Anim ici
             timer += Time.deltaTime;
             yield return null;
         }
         float delay = Random.Range(delayMinMax.x, delayMinMax.y);
+        meteoriteEntity.SetActive(false);
         StartCoroutine(brush.CircleDraw(SO_Tile, pLocation, pRadius, tilemaps, delay));
     }
 }
